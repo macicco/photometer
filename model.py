634c97132f68862555e0f36bbe01a47b67baad9c
Stefan Nelson <pulsestaysconstant@gmail.com>
2016-12-05 10:14:13 -0600

    Added attributes to model

--- model.py
+++ model.py
@@ -1,7 +1,13 @@
 from __future__ import print_function, division
 import traitlets
 
-class Photometry(traitlets.HasTraits):
+class AperturePhotometry(traitlets.HasTraits):
+    
+    aperture_radius = traitlets.Integer()
+    annulus_inner_radius = traitlets.Integer()
+    annulus_outer_radius = traitlets.Integer()
+    aperture_net_counts = traitlets.Float()
+    
     def __init__(self):
         super(Photometry, self).__init__(description='Photometry model')
         pass
